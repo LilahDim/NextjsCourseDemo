73e795d23e25b29ea505fe0a7bfe9678f3023315
James Dimacali <frostbitezebra421@gmail.com>
2023-05-04 17:10:21 -0700

    ready for deployment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://JamesDimacali:Shadowzebra421@cluster0.sdom5mf.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
